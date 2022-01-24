9f08eaa4a9f73affa9e32e99bbd36b35723d9bb3
Konstantin Berezko <berezkok72@gmail.com>
2022-01-24 22:00:00 +0500

    fix style nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,20 +14,37 @@ const Wrapper = styled(Container)`
 const Nav = styled.nav`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  /* margin: 0 auto; */
+
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+  @media (max-width: 375px) {
+    gap: 10px;
+  }
 `;
 const Logo = styled.img`
   max-width: 34px;
-  margin-right: auto;
-
-  @media (max-width: 375px) {
-    margin-right: 10px;
-  }
 `;
 const NavList = styled.ul`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  gap: 5px;
+  flex-grow: 1;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: baseline;
+    flex-grow: 0;
+    /* visibility: hidden; */
+    transform: translateY(-50%);
+  }
 `;
 const Burger = styled.div`
+  display: none;
   margin-left: auto;
 `;
 
@@ -38,8 +55,8 @@ const Header = () => {
         <Nav>
           <Logo src={appleLogo} alt="apple" />
           <NavList>
-            {env.nav.map(item => (
-              <NavItem href={item[0]} name={item[1]} />
+            {env.nav.map((item, i) => (
+              <NavItem key={i} href={item[0]} name={item[1]} />
             ))}
           </NavList>
           <Burger />
--- src/components/Header/NavItem.js
+++ src/components/Header/NavItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Item = styled.li`
-  margin-right: 30px;
   white-space: nowrap;
 
   & > a {
