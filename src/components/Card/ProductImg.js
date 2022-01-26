ae33d01d565446b85f97bc24aeb4db759a003083
Konstantin Berezko <berezkok72@gmail.com>
2022-01-26 18:27:07 +0500

    add img animation

--- src/components/Card/ProductImg.js
+++ src/components/Card/ProductImg.js
@@ -1,8 +1,20 @@
-import React, { useContext } from 'react';
-import styled from 'styled-components';
+import React, { useContext, useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 import { useSelector } from 'react-redux';
 import { selectPresent } from '../../store/goodSlice';
 import { CurrentModelContext } from '../Context';
+import env from '../../env.json';
+
+const transitionDuration = env.transitionDuration;
+
+const show = keyframes`
+  0% { opacity: 0 }
+  100% { opacity: 1 }
+`;
+const hide = keyframes`
+  0% { opacity: 1 }
+  100% { opacity: 0 }
+`;
 
 const CardImg = styled.figure`
   max-width: 300px;
@@ -16,6 +28,10 @@ const CardImg = styled.figure`
   & > img {
     width: 230px;
     max-width: none;
+    animation-name: ${props => (props.change === 'show' ? show : hide)};
+    animation-timing-function: ease-in-out;
+    animation-duration: ${transitionDuration};
+    animation-fill-mode: forward;
   }
 `;
 
@@ -24,11 +40,24 @@ const ProductImg = () => {
     currentModel: { currentImg },
   } = useContext(CurrentModelContext);
 
+  const [nextImg, setNextImg] = useState('');
+  const [changeImg, setChangeImg] = useState('show');
+
+  useEffect(() => {
+    if (currentImg !== nextImg) {
+      setChangeImg('hide');
+      setTimeout(() => {
+        setNextImg(currentImg);
+        setChangeImg('show');
+      }, transitionDuration);
+    }
+  }, [currentImg, nextImg]);
+
   const altText = useSelector(selectPresent).imgAlt;
 
   return (
-    <CardImg>
-      <img src={currentImg} alt={altText} />
+    <CardImg change={changeImg}>
+      <img src={nextImg} alt={altText} />
     </CardImg>
   );
 };
